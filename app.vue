dc356c9c90c90088bd51ce713e74887024c0b73a
yangjihong <jihongyang@shannonai.com>
2023-12-21 16:01:26 +0800

    feat: 测试多语言

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
 	<NuxtPage />
+	<h1>{{ $t('name') }}</h1>
+	<button @click="$i18n.locale = 'zh'">中文</button>
+	<button @click="$i18n.locale = 'en'">英文</button>
 </template>
 <script lang="ts" setup>
 import { APIs } from './service/apiList'
