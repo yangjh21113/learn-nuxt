fa265b4166e52963e51c5075263176a8d58b1b82
yangjihong <jihongyang@shannonai.com>
2023-12-21 15:57:51 +0800

    feat: 测试请求

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
 <template>
 	<NuxtPage />
 </template>
+<script lang="ts" setup>
+import { APIs } from './service/apiList'
+import type AxiosRequestError from './service/error'
+import $api from './service/webRequest'
+
+$api.get(APIs.login)
+	.then(() => {
+		//
+	})
+	.catch((err: AxiosRequestError) => {
+		console.dir(err)
+	})
+</script>
 <style>
 @import url('ant-design-vue/dist/reset.css');
 </style>
