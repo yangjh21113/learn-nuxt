a150b26432ee29de2179b3e3fb97b18898225b87
yangjihong <jihongyang@shannonai.com>
2023-12-21 11:47:18 +0800

    feat: 使用 pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,5 @@ export default defineNuxtConfig({
 			tailwindcss: {},
 		},
 	},
+	modules: ['@pinia/nuxt'],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
-    <div class="box">hello</div>
-    <div class="text-[43px] text-[red]">323</div>
+  <div class="box">hello</div>
+  <div class="text-[43px] text-[red]" @click="changeName">{{ store.name }}</div>
+  <div></div>
 </template>
 <script lang="ts" setup>
+import { useStore } from '../store/index';
+
+const store = useStore();
+function changeName(): void {
+  store.name = 'new Name';
+}
 </script>
 <style lang="scss" scoped>
 @import url(../assets/css/common.scss);
